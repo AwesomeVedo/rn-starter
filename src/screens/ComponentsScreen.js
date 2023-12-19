1be77ca6c7fa206bc584340e9961e6e8150a0ef3
Miguel Acevedo <581miguelacevedo@gmail.com>
2023-12-19 03:35:44 -0500

    Added ListScreen.js file.

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Text, StyleSheet, View } from 'react-native';
 
-const name = "Miguel";
+const name = "Miguel Acevedo";
 const ComponentsScreen = () => {
 
 
     return (
         <View>
             <Text style={styles.textTitle} >Getting started with native react!</Text>
-            <Text style={styles.textDefault}>My name is {name}</Text>
+            <Text style={styles.textSubHeading}>My name is {name}</Text>
         </View>
     )
 };
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
     textTitle: {
         fontSize:45
     },
-    textDefault: {
+    textSubHeading: {
         fontSize:20
     }
 });
