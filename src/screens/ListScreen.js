3edc44cabba74cb04d60d0f4e58abccaf95d5c3b
Miguel Acevedo <581miguelacevedo@gmail.com>
2023-12-21 21:05:12 -0500

    Added excerise 2. (Udemy Tutorial)

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,21 +3,22 @@ import { Text, StyleSheet, View, FlatList} from "react-native";
 
 const ListScreen = () => {
 const friends = [
-    { name: 'friend #1'},
-    { name: 'friend #2'},
-    { name: 'friend #3'},
-    { name: 'friend #4'},
-    { name: 'friend #5'},
-    { name: 'friend #6'},
-    { name: 'friend #7'},
-    { name: 'friend #8'},
-    { name: 'friend #9'},
+    { name: 'friend #1', age: 28},
+    { name: 'friend #2', age: 27},
+    { name: 'friend #3', age: 24},
+    { name: 'friend #4', age: 28},
+    { name: 'friend #5', age: 35},
+    { name: 'friend #6', age: 22},
+    { name: 'friend #7', age: 74},
+    { name: 'friend #8', age: 43},
+    { name: 'friend #9', age: 30},
 ];
     return (
-        <FlatList 
+        <FlatList
+            keyExtractor={friend => friend.name}
             data={friends}
             renderItem={({ item }) => {
-                return <Text>{item.name}</Text>
+                return <Text style={styles.textStyle}>{item.name} – Age {item.age}</Text>
             } }
         />
     );
@@ -25,6 +26,12 @@ const friends = [
     
 
 }
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+
+    textStyle: {
+        marginVertical: 10
+    }
+
+});
 
 export default ListScreen;
