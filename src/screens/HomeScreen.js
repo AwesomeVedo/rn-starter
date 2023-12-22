5984655225d08bd7007bcf555766dacec7a9eb94
Miguel Acevedo <581miguelacevedo@gmail.com>
2023-12-22 01:41:49 -0500

    Added navigation ability. (Tutorial)

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,32 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Getting started with React Native</Text>;
+const HomeScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text style={styles.text}>Getting started with React Native</Text>
+      <Button 
+        onPress={() => navigation.navigate("Components")}
+        title="Go to Components Demo"
+      />
+      <Button 
+        onPress={() => navigation.navigate("List")}
+        title="Go to List Demo"
+      />
+    </View>
+    
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
   },
+  buttonStyle: {
+    borderWidth:2,
+    borderColor:'red',
+    padding:10
+  }
 });
 
 export default HomeScreen;
