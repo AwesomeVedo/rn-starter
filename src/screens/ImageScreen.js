de622f943c1a48fcbfff75066998b4de6725bea0
Miguel Acevedo <581miguelacevedo@gmail.com>
2023-12-27 17:26:23 -0500

    Added imageScreen.js (Tutorial)

---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,14 @@
+import react from "react";
+import {View, StyleSheet, Text} from 'react-native';
+
+const ImageScreen = () => {
+    return (
+        <Text>Image Screen</Text>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default ImageScreen;
