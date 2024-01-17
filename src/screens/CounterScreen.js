07bea3f55e19872ae50bb19fc13bb665fcb793c4
Miguel Acevedo <581miguelacevedo@gmail.com>
2024-01-17 18:04:52 -0500

    Working on understanding reducers. (tutorial)

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,38 +1,58 @@
 import React, { useState, useReducer } from 'react';
-import { Text, View, StyleSheet, Button } from "react-native";
+import { Text, TextInput, View, StyleSheet, Button } from "react-native";
 
-const reducer = (state, action) => {
-    return {...state, counter: state.counter + action.counterChange};
-}
+
 
 const CounterScreen = () => {
-    //const [counter, setCounter] = useState(0);
-   
-    const [state, dispatch] = useReducer(reducer, {counter:0});
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'INCREASE': return {...state, count: state.count + state.step};
+            case 'DECREASE': return {...state, count: state.count - state.step};
+            case 'SET_STEP': return !isNaN(action.inputNumber) ? {...state, step: action.inputNumber} : {...state, step:1};
+            default: return {...state, step:1};
+            break;
+        }
+    }
+
+    const [state, dispatch] = useReducer(reducer, {count:1, step:1});
 
     return (
         <View>
             <Button
                 title="Increase"
                 onPress={() => {
-                    //setCounter(counter + 1)
-                    dispatch({counterChange:1});
+                    dispatch({type: 'INCREASE'});
                 }}
             />
             <Button 
                 title="Decrease"
                 onPress={() => {
-                    //setCounter(counter - 1)
-                    dispatch({counterChange:-1});
+                    dispatch({type: 'DECREASE'});
                 }}
             />
-            <Text>Current Count: {state.counter}</Text>
+            <Text>Set Step</Text>
+            <TextInput
+                style={styles.input}
+                keyboardType="numeric"
+                //value={state.count}
+                onChangeText={(thisValue) => {
+                    // console.log(parseInt(thisValue));
+                    dispatch({type: 'SET_STEP', inputNumber: parseInt(thisValue)});
+                }}
+            ></TextInput>
+            <Text>Current Count: {state.count}</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    input: {
+        eight: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    }
 });
 
 export default CounterScreen;
